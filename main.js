86ac027b81dac91102c84a66d6676281cadd37d2
rb1econ <rb1econ@gmail.com>
2015-03-27 08:46:15 -0600

    second porb done

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
+
 var alphabetSoup = function (string){
+	// This rocks because there is no sort method called. 
+	// The tized array is populated with letters that are 
+	// already alphabetized because I map over the alphbet array first.
+	// Pretty cool.
 	var splitd = string.split('');
 	var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-	var numberatized=[];
+	var tized=[];
 	alphabet.map(function(letter){
 		splitd.map(function(stringLet){
 			if(letter === stringLet){
-				numberatized.push(letter);
+				tized.push(letter);
 			}
 		});
 
 	});
-	return numberatized;
+	return tized;
 };
 
-console.log(alphabetSoup("howdeosapersonbehavereationalconditional"));
+// console.log(alphabetSoup("howdeosapersonbehavereationalconditional"));
 			
 
+var vowelCount = function(str){
+	var splitd = str.split('');
+	var counter = 0;
+	splitd.map(function(let){
+		if(let === 'a' || let ==='i'||let ==='e'||let ==='i'||let ==='o'||let ==='u'||let ==='y'){
+			console.log(let);
+			counter++;
+		}
+	});
+	return counter
+};
 
-			
+console.log(vowelCount('misalabam asis arkan sass ipmiss ouripi'))
