ae64400e3e9daa8d2ea4a3fd46f4f13e348cbdea
rb1econ <rb1econ@gmail.com>
2015-03-27 11:45:33 -0600

    yep, still trying

--- main.js
+++ main.js
@@ -36,65 +36,13 @@ var vowelCount = function(str){
 // console.log(vowelCount('mIsalAbam AsisArkAn sAss ipmiss ouripI'))
 
 
-// 1 5 7 9 11
-var coinDet = function(num){
-	var ones;
-	var fives;
-	var sevens;
-	var nines;
-	if(num>=11){
-		var elevens = Math.floor(num/11);
-		// console.log('elevens',elevens);
-		var afterEleven = num - (elevens*11)
-		if(afterEleven>=9){
-			enterNine(afterEleven);
-		}
-		else if(afterEleven>=5){
-			enterFive(afterEleven);
-		}
-		else{
-			ones = num;
-		}
-	}
-	else if(num>=9){
-		enterNine(num);
-		var enterNine = function(afterEleven){
-			nines = Math.floor(afterEleven/9);
-			var afterNine = afterEleven - (nines*9);
-			if(afterNine>=5){}
-			else{ones = afterNine;}
-		};
-	}
-	else if (num>=7){
-		enterSeven(num);
-		var enterSeven = function(afterNine){
-			sevens = Math.floor(afterNine/7);
-			afterSeven = afterNine - (sevens*7);
-		};
-	}
-	else if(num>=5){
-		enterFive(num);
-		var enterFive = function(afterSeven){
-			fives = Math.floor(newNum/5);
-			var afterFive = afterSeven - (fives*5);
-			ones = afterFive;
-		};
-	}
-	else{
-		ones = num;
-	}
-	// console.log('ones',ones)
-	return elevens + nines + fives + ones;
-};
-// console.log(coinDet(12));
-
-
-
-
+// A function that tells you the minimum number of coins your total money value can be converted to if the coins are in denominations of 1, 5, 7, 9, 11. First pull out the 11s, then 9s if any exist etc...
 var romanCoin = function(num){
-	var ones;
-	var sevens;
-	var nines;
+	var ones=0;
+	var fives=0;
+	var sevens=0;
+	var nines=0;
+	var elevens=0;
 	if(num>=11){
 		var elevens = Math.floor(num/11);
 		var afterEleven = num - elevens*11;
@@ -120,13 +68,28 @@ var romanCoin = function(num){
 	else if(num>=7){
 		enterSeven(num);
 		function enterSeven(afterNine){
-			sevens = Math.floor(afterNine);
+			sevens = Math.floor(afterNine/7);
 			var afterSeven = afterNine - sevens*7;
 			ones = afterSeven;
 		}
 	}
+	else if(num>=5){
+		enterFive(num);
+		function enterFive(afterSeven){
+			fives = Math.floor(afterSeven/5);
+			var afterFive = afterSeven - fives*5;
+			ones = afterFive;
+		};
+	}
 	else{ones = num}
+	nines = parseInt(nines);
+	sevens = parseInt(sevens);
+	console.log('11', elevens, 'nines', nines, '7', sevens, '5', fives, '1', ones)
 	return elevens + nines + sevens + ones;
 };
 
-console.log(romanCoin(28));
+console.log('typeof romanCoin()',typeof romanCoin(28));
+
+var theOutput = romanCoin(29);
+
+$('.coiner').append('<p>'+parseInt(theOutput)+'</p>');
