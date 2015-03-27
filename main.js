f7b19dd90f8e36bd2520041632cf7803eaf8e4b8
rb1econ <rb1econ@gmail.com>
2015-03-27 09:26:29 -0600

    tough one

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ var alphabetSoup = function (string){
 			
 
 var vowelCount = function(str){
-	var splitd = str.split('');
+	var splitd = str.toLowerCase().split('');
 	var counter = 0;
 	splitd.map(function(let){
 		if(let === 'a' || let ==='i'||let ==='e'||let ==='i'||let ==='o'||let ==='u'||let ==='y'){
@@ -33,4 +33,35 @@ var vowelCount = function(str){
 	return counter
 };
 
-console.log(vowelCount('misalabam asis arkan sass ipmiss ouripi'))
+console.log(vowelCount('mIsalAbam AsisArkAn sAss ipmiss ouripI'))
+
+
+// 1 5 7 9 11
+var coinDet = function(num){
+	var coinCounter = 0;
+	if(num/11>=1){
+		var elevens = num/11;
+		var newNum = num - (elevens*11)
+		if(newNum>=9){
+			enterNine();
+		}
+		else if(newNum>=5){
+			enterFive();
+		}
+		else{return num;}
+	}
+	else if(num/9>=1){
+		enterNine();
+		var enterNine = function(newNum){
+			var nine = newNum/9;
+
+		};
+	}
+	else if(num/5>=1){
+		var enterFive = function(){
+
+		};
+	}
+	else{return num;}
+	return parseInt(elevens) + parseInt(nines) + parseInt(fives) + parseInt(ones);
+};
